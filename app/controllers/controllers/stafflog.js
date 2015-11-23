91336d3ac1a6d42bd6e77bc57fec4c7bd189feb3
unknown <dennis.stammer72@hotmail.com>
2015-11-23 21:55:12 +0100

    first

--- app/controllers/controllers/stafflog.js
+++ app/controllers/controllers/stafflog.js
@@ -1,23 +1,39 @@
-var utils = require('../../../utils');
-
 module.exports = function (opts) {
     var staffLogModel = opts.models.StaffLog;
         
     return {
         "post#stafflogs/create" : function (req, res) {
 			var type = req.body.type,
-				timestamp = req.body.timestamp;
+				timestamp = req.body.timestamp,
+				sitesafe = req.body.sitesafe;
 
-			var stafflog = new staffLogModel();
-			stafflog.type = type;
-			stafflog.timestamp = (timestamp)?timestamp * 1000:new Date().getTime();
+			console.log('here');
+			console.log(req.body);
 
-			stafflog.save(function(err, nlog){
+			staffLogModel.findOne({sitesafe: sitesafe}).sort({timestamp: -1}).limit(1).exec(function(err, log){
 				if(err){
-					return res.json({ success : false, error : "Internal server error" });
+					return res.json({success : false, error : "Internal server eror" });
 				}
 
-				return res.json({success : true});
+				console.log(log);
+
+				if(log && log.type == type){
+					if(type == "IN") return res.json({success : false, error : "A staff with the same SiteSafe Passport is already signed in."});
+					else return res.json({success : false, error : "A staff with the same SiteSafe Passport is already signed out."});
+				}
+
+				var stafflog = new staffLogModel();
+				stafflog.type = type;
+				stafflog.sitesafe = sitesafe;
+				stafflog.timestamp = (timestamp)?timestamp * 1000:new Date().getTime();
+
+				stafflog.save(function(err, nlog){
+					if(err){
+						return res.json({ success : false, error : "Internal server error" });
+					}
+
+					return res.json({success : true});
+				});
 			});
         },
 
--- config.js
+++ config.js
@@ -3,4 +3,5 @@ var mongoose = require("mongoose");
 module.exports.devPort = process.env.PORT || 9010;
 module.exports.prodPort = process.env.PORT || 80;
 
-module.exports.dbConnection = mongoose.createConnection("mongodb://heroku_tbgr3kv2:730kmucarmsspf813nblnumugq@ds053194.mongolab.com:53194/heroku_tbgr3kv2");
+//module.exports.dbConnection = mongoose.createConnection("mongodb://heroku_tbgr3kv2:730kmucarmsspf813nblnumugq@ds053194.mongolab.com:53194/heroku_tbgr3kv2");
+module.exports.dbConnection = mongoose.createConnection("mongodb://localhost:27017/local");
